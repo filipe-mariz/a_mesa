a934e721f968c753f86de5f54da5cb73befafb67
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-31 16:41:36 -0300

    feat: created model of Volunteer

--- app/Models/Volunteer.ts
+++ app/Models/Volunteer.ts
@@ -1,13 +1,48 @@
+import uuid from 'uuid/v4'
+import Hash from '@ioc:Adonis/Core/Hash'
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeCreate, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Volunteer extends BaseModel {
+  public static connection = 'pg'
+  public static selfAssignPrimaryKey = true
+
   @column({ isPrimary: true })
-  public id: number
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column()
+  public password: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public phone: string
+
+  @column()
+  public help_type: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @beforeCreate()
+  public static assignUuid(volunteers: Volunteer) {
+    volunteers.id = uuid()
+  }
+
+  @beforeSave()
+  public static async hashPassword(volunteer: Volunteer) {
+    if (volunteer.$dirty.password) {
+      volunteer.password = await Hash.hash(volunteer.password)
+    }
+  }
 }
