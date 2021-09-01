a6a8d35f18077137aa78f64904cd902f2f0022f7
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-08-31 21:17:04 -0300

    delete: deleted line not used in routes file

--- start/routes.ts
+++ start/routes.ts
@@ -16,15 +16,5 @@ Route.group(() => {
 
 }).prefix('/tenants')//.middleware('tenantHandler')
 
-/* Route.group(() => {
-  Route.group(() => {
-    Route.post('/', 'VoluntariesController.register')
-    Route.get('/', 'VoluntariesController.index')
-    Route.get('/:id', 'VoluntariesController.show')
-    Route.get('/forgot-password/:id', 'VoluntariesController.forgotPassword')
-    Route.put('/:id', 'VoluntariesController.update')
-    Route.put('/update-password/:id', 'VoluntariesController.updatePassword')
-  }).prefix('/voluntary')
-}).prefix('/tenants').middleware('tenantHandler') */
 
 Route.post('/', 'VolunteersController.register')
