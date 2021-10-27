88ace20c98a3c45d89c27a62d8af832e726fe781
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-10-27 09:20:38 -0300

    feat: added model of volunteer helper

---
+++ app/Models/VolunteerHelper.ts
@@ -0,0 +1,38 @@
+import uuid from 'uuid/v4'
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Help from './Help'
+
+export default class VolunteerHelper extends BaseModel {
+  public static connection = 'pg'
+  public static selfAssignPrimaryKey = true
+
+  @column({ isPrimary: true })
+  public id: string;
+
+  @column()
+  public help_id: string;
+
+  @column()
+  public date_of_help: string;
+
+  @column()
+  public description: string;
+
+  @column()
+  public image_link: string;
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasOne(() => Help)
+  public help: HasOne<typeof Help>
+
+  @beforeCreate()
+  public static assignUuid(volunteerHelper: VolunteerHelper) {
+    volunteerHelper.id = uuid()
+  } 
+}
