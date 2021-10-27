ca33da8d0a4e04362848f1930568d0c5c6273ada
Filipe Mariz <filipeferreiramariz@outlook.com>
2021-10-27 16:03:03 -0300

    feat: added controller for login

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -3,6 +3,7 @@ import argon2 from 'phc-argon2';
 import ApiToken from 'App/Models/ApiToken';
 import Volunteer from 'App/Models/Volunteer';
 import Needy from 'App/Models/Needy';
+import ProfessionalVolunteer from 'App/Models/ProfessionalVolunteer';
 
 export default class LoginController {
   public async volunteer({ request, response }) {
@@ -36,6 +37,43 @@ export default class LoginController {
     const token = await argon2.hash(`${data.email}-${data.password}`)
 
     const creationBody = { token, reference_id: volunteer.id }
+
+    const apiToken = await ApiToken.create(creationBody)
+
+    return apiToken
+  }
+
+  public async professioal({ request, response }) {
+    const requiredFields = ['email', 'password']
+
+    const data = request.only(requiredFields)
+
+    for (const field of requiredFields) {
+      if (!data[field]) {
+        return response.status(400).json({
+          MissingParamError: `MISSING PROPERTY ${field} ON BODY`,
+          Solution: 'Adding this field to the body may solve the problem',
+        })
+      }
+    }
+
+    ProfessionalVolunteer.connection = request.tenantConnection
+
+    const professionalVolunteer = await ProfessionalVolunteer.findByOrFail('email', data.email)
+
+    const isPasswordValid = await argon2.verify(professionalVolunteer.password, data.password);
+
+    if (!isPasswordValid) {
+      return response.status(400).json({
+        InvalidParamError: `INVALID PROPERTY password ON BODY`,
+        Solution:
+          'You provided a wrong password, be sure to check if you are using the correct one',
+      })
+    }
+
+    const token = await argon2.hash(`${data.email}-${data.password}`)
+
+    const creationBody = { token, reference_id: professionalVolunteer.id }
 
     const apiToken = await ApiToken.create(creationBody)
 
